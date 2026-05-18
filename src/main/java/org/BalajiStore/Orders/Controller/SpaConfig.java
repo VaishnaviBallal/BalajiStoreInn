acbca9a5aed1edf4814b37399a437dac6ced3031
Vaishnavi Ballal <va20680341@wipro.com>
2026-05-18 22:04:18 +0530

    spa controller for qr code

--- src/main/java/org/BalajiStore/Orders/Controller/SpaConfig.java
+++ src/main/java/org/BalajiStore/Orders/Controller/SpaConfig.java
@@ -1,7 +1,6 @@
 package org.BalajiStore.Orders.Controller;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,10 +10,19 @@ public class SpaConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
-        registry.addViewController("/{spring:[^.]*}")
+        registry.addViewController("/")
                 .setViewName("forward:/index.html");
 
-        registry.addViewController("/**/{spring:[^.]*}")
+        registry.addViewController("/customer-menu")
+                .setViewName("forward:/index.html");
+
+        registry.addViewController("/admin-orders")
+                .setViewName("forward:/index.html");
+
+        registry.addViewController("/qr-generator")
+                .setViewName("forward:/index.html");
+
+        registry.addViewController("/home")
                 .setViewName("forward:/index.html");
     }
 }
